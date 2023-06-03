b056a2d6d981ebe7a4460fc8040b93ff8863289f
salmaaghisn <97302116+salmaaghisn@users.noreply.github.com>
2023-06-03 15:10:30 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import navbar from'./components/navbar.vue'
+  import respon from'./components/respon.vue'
+</script>
+
+<template>
+  <header>
+    <navbar>
+
+    </navbar>
+  </header>
+
+  <main>
+    <respon>
+      
+    </respon>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
